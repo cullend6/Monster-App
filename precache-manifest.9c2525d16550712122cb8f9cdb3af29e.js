46bf9ceff150d7162b2957a8418a8915464f84e4
cullend6 <dan.cullen95@gmail.com>
2019-11-12 21:47:47 +1100

    Updates

---
+++ precache-manifest.9c2525d16550712122cb8f9cdb3af29e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28959c63590c575fbbac0b6fee94c61f",
+    "url": "/Monster-App/index.html"
+  },
+  {
+    "revision": "f725526e680938cf2b42",
+    "url": "/Monster-App/static/css/main.6e14d448.chunk.css"
+  },
+  {
+    "revision": "90d86812a41173d4994e",
+    "url": "/Monster-App/static/js/2.211a65be.chunk.js"
+  },
+  {
+    "revision": "f725526e680938cf2b42",
+    "url": "/Monster-App/static/js/main.8eb7ff17.chunk.js"
+  },
+  {
+    "revision": "a8abaa23a5d7eaffb927",
+    "url": "/Monster-App/static/js/runtime-main.c155da5e.js"
+  }
+]);
